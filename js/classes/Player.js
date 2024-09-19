d267e14e3521f08cd99e19628dad07cf678b9634
JoelTurtiainen <joel.turtiainen@gmail.com>
2024-09-19 13:53:28 +0300

    initial

---
+++ js/classes/Player.js
@@ -0,0 +1,104 @@
+class Player extends Sprite {
+	constructor({ position, collisionBlocks, imageSrc, frameRate, frameBuffer, scale = 0.5 }) {
+		super({ imageSrc, frameRate, scale });
+		this.position = position;
+		this.frameBuffer = frameBuffer;
+		this.velocity = {
+			x: 0,
+			y: 1,
+		};
+		this.collisionBlocks = collisionBlocks;
+		this.hitbox = {
+			position: {
+				x: this.position.x,
+				y: this.position.y,
+			},
+			width: 10,
+			height: 10,
+		};
+	}
+
+	update() {
+		this.updateFrames();
+		this.updateHitbox();
+
+		// draws out the image
+		c.fillStyle = 'rgba(0, 255, 0, 0.2)';
+		c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+		// draws out the hitbox
+		c.fillStyle = 'rgba(255, 0, 0, 0.2)';
+		c.fillRect(this.hitbox.position.x, this.hitbox.position.y, this.hitbox.width, this.hitbox.height);
+
+		this.draw();
+
+		this.position.x += this.velocity.x;
+		this.checkForHorizontalCollisions();
+		this.applyGravity();
+		this.checkForVerticalCollisions();
+	}
+
+	updateHitbox() {
+		this.hitbox = {
+			position: {
+				x: this.position.x,
+				y: this.position.y,
+			},
+			width: 10,
+			height: 10,
+		};
+	}
+
+	checkForHorizontalCollisions() {
+		for (let i = 0; i < this.collisionBlocks.length; i++) {
+			const collisionBlock = this.collisionBlocks[i];
+
+			if (
+				collision({
+					object1: this,
+					object2: collisionBlock,
+				})
+			) {
+				if (this.velocity.x > 0) {
+					this.velocity.x = 0;
+					this.position.x = collisionBlock.position.x - this.width - 0.01;
+					break;
+				}
+				if (this.velocity.x < 0) {
+					this.velocity.x = 0;
+					this.position.x = collisionBlock.position.x + collisionBlock.width + 0.01;
+					break;
+				}
+			}
+		}
+	}
+
+	applyGravity() {
+		this.position.y += this.velocity.y;
+		this.velocity.y += gravity;
+	}
+
+	checkForVerticalCollisions() {
+		for (let i = 0; i < this.collisionBlocks.length; i++) {
+			const collisionBlock = this.collisionBlocks[i];
+
+			if (
+				collision({
+					object1: this,
+					object2: collisionBlock,
+				})
+			) {
+				if (this.velocity.y > 0) {
+					this.velocity.y = 0;
+					this.position.y = collisionBlock.position.y - this.height - 0.01;
+					break;
+				}
+				if (this.velocity.y < 0) {
+					this.velocity.y = 0;
+					this.position.y = collisionBlock.position.y + collisionBlock.height + 0.01;
+					break;
+				}
+			}
+		}
+	}
+}
